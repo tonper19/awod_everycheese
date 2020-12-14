df76e219a81d25ff3aad01a1ea3aca6f7d30bc82
Tony Perez <tonper19@gmail.com>
2020-12-14 20:06:27 +0200

    0045. Implement cheese country of origin

--- everycheese/config/settings/base.py
+++ everycheese/config/settings/base.py
@@ -81,6 +81,7 @@ THIRD_PARTY_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    "django_countries",  # CountryField 20201214
 ]
 
 LOCAL_APPS = [
--- everycheese/everycheese/cheeses/models.py
+++ everycheese/everycheese/cheeses/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
 from autoslug import AutoSlugField
+from django_countries.fields import CountryField
 
 
 class Cheese(TimeStampedModel):
@@ -19,6 +20,7 @@ class Cheese(TimeStampedModel):
                                 choices=Firmness.choices,
                                 default=Firmness.UNSPECIFIED
                                 )
+    country_of_origin = CountryField("Country of Origin", blank=True)
 
     def __str__(self):
         return self.name
