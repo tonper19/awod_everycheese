8f42a43ba85b3c35a71ba2e188fbfb5ae0cffe9f
Tony Perez <tonper19@gmail.com>
2020-12-17 15:32:55 +0200

    0055. Add cheese update view, modify template, add flag to list template

--- everycheese/everycheese/cheeses/urls.py
+++ everycheese/everycheese/cheeses/urls.py
@@ -14,6 +14,11 @@ urlpatterns = [
         name="add"
     ),
     path(
+        route='<slug:slug>/update/',
+        view=views.CheeseUpdateView.as_view(),
+        name='update'
+    ),
+    path(
         route="<slug:slug>/",
         view=views.CheeseDetailView.as_view(),
         name="detail"
--- everycheese/everycheese/cheeses/views.py
+++ everycheese/everycheese/cheeses/views.py
@@ -4,6 +4,7 @@ from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
+    UpdateView,
 )
 from .models import Cheese
 
@@ -28,3 +29,13 @@ class CheeseCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
+
+
+class CheeseUpdateView(LoginRequiredMixin, UpdateView):
+    model = Cheese
+    fields = ['name',
+              'description',
+              'firmness',
+              'country_of_origin',
+              ]
+    action = "Update"
